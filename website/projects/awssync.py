5f37e060df4b31d5c054be40beea9a6f8f34ffae
Jer111 <j.vanoort7@gmail.com>
2023-02-22 14:37:35 +0100

    Added get mailinglist to the awssync file

--- website/projects/awssync.py
+++ website/projects/awssync.py
@@ -1,5 +1,6 @@
 """Framework for synchronisation with Amazon Web Services (AWS)."""
-
+from mailing_lists.models import MailingList
+from django.db import models
 
 class AWSSync:
     """Synchronise with Amazon Web Services."""
@@ -7,7 +8,8 @@ class AWSSync:
     def __init__(self):
         """Create an AWSSync instance."""
         print("Created AWSSync instance")
-
+        
+    
     def button_pressed(self):
         """
         Print debug message to show that the button has been pressed.
@@ -15,4 +17,21 @@ class AWSSync:
         :return: True if function executes successfully
         """
         print("Pressed button")
+        print(self.get_all_mailing_lists())
+        print(self.get_all_managers())
+
         return True
+
+    def get_all_mailing_lists(self):
+        """
+        Get all mailing lists from the database.
+        """
+        mailing_lists = MailingList.objects.all()
+        mailing_list_names = [mailing_list.email_address for mailing_list in mailing_lists]
+        print(mailing_list_names)
+        return mailing_list_names
+
+
+    
+
+        
