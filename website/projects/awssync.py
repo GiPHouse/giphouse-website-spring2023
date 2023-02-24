ad458e65b978aae5d08c7d16db77f9cf2ece1c4d
Jer111 <j.vanoort7@gmail.com>
2023-02-24 11:39:41 +0100

    Added get_teamid_from_email

--- website/projects/awssync.py
+++ website/projects/awssync.py
@@ -1,5 +1,7 @@
 from mailing_lists.models import MailingList
 
+from projects.models import Project
+
 """Framework for synchronisation with Amazon Web Services (AWS)."""
 
 
@@ -17,7 +19,9 @@ class AWSSync:
         :return: True if function executes successfully
         """
         print("Pressed button")
-
+        temp_list = self.get_all_mailing_lists()
+        print(temp_list)
+        print(temp_list[0])
         return True
 
     def get_all_mailing_lists(self):
@@ -29,3 +33,14 @@ class AWSSync:
         mailing_lists = MailingList.objects.all()
         mailing_list_names = [ml.email_address for ml in mailing_lists]
         return mailing_list_names
+
+    def get_teamid_from_email(self, email_address):
+        """
+        Get the team ID from an email address.
+
+        :param email_address: Email address of the team
+        :return: Team ID
+        """
+        project = Project.objects.get(email=email_address)
+        return project.github_team_id
+
