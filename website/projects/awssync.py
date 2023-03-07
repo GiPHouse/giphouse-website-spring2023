5c8bdd7f285955ed6a7609e6a4fb1a3f0f309a74
Jer111 <j.vanoort7@gmail.com>
2023-03-07 13:07:32 +0100

    Added create_team_ou with tests

--- website/projects/awssync.py
+++ website/projects/awssync.py
@@ -12,6 +12,7 @@ class AWSSync:
         """Create an AWSSync instance."""
         self.logger = logging.getLogger("django.aws")
         self.org_info = None
+        self.iterationOU_info = None
         self.fail = False
         self.logger.info("Created AWSSync instance.")
         self.client = boto3.client("organizations")
@@ -53,9 +54,33 @@ class AWSSync:
                 Name=f"Course Iteration {iteration_id}",
             )
             self.logger.info(f"Created an OU for course iteration {iteration_id}.")
+            self.iterationOU_info = response["OrganizationalUnit"]
             return response["OrganizationalUnit"]["Id"]
         except ClientError as error:
             self.fail = True
             self.logger.error(f"Something went wrong creating an OU for course iteration {iteration_id}.")
+            self.logger.debug(f"{error}")
+            self.logger.debug(f"{error.response}")
+
+    def create_team_OU(self, team_id):
+        """
+        Create an OU for the team.
+
+        :param team_id: The ID of the team
+
+        :return: The ID of the OU
+        """
+        if self.iterationOU_info is None:
+            self.create_course_iteration_OU(1)  # Needs to be figured out
+        try:
+            response = self.client.create_organizational_unit(
+                ParentId=self.iterationOU_info["Id"],
+                Name=f"{team_id}",
+            )
+            self.logger.info(f"Created an OU for team {team_id}.")
+            return response["OrganizationalUnit"]["Id"]
+        except ClientError as error:
+            self.fail = True
+            self.logger.error(f"Something went wrong creating an OU for team {team_id}.")
             self.logger.debug(f"{error}")
             self.logger.debug(f"{error.response}")
--- website/projects/tests/test_awssync.py
+++ website/projects/tests/test_awssync.py
@@ -21,56 +21,53 @@ class AWSSyncTest(TestCase):
         return_value = self.sync.button_pressed()
         self.assertTrue(return_value)
 
-
     def mock_api(self, operation_name, kwarg):
-            if operation_name == "CreateOrganization":
-                raise ClientError(
-                    {
-                        "Error": {
-                            "Message": "The AWS account is already a member of an organization.",
-                            "Code": "AlreadyInOrganizationException",
-                        },
-                        "ResponseMetadata": {
-                            "RequestId": "ffffffff-ffff-ffff-ffff-ffffffffffff",
-                            "HTTPStatusCode": 400,
-                            "HTTPHeaders": {
-                                "x-amzn-requestid": "ffffffff-ffff-ffff-ffff-ffffffffffff",
-                                "content-type": "application/x-amz-json-1.1",
-                                "content-length": "111",
-                                "date": "Sun, 01 Jan 2023 00:00:00 GMT",
-                                "connection": "close",
-                            },
-                            "RetryAttempts": 0,
-                        },
+        if operation_name == "CreateOrganization":
+            raise ClientError(
+                {
+                    "Error": {
                         "Message": "The AWS account is already a member of an organization.",
+                        "Code": "AlreadyInOrganizationException",
                     },
-                    "create_organization",
+                    "ResponseMetadata": {
+                        "RequestId": "ffffffff-ffff-ffff-ffff-ffffffffffff",
+                        "HTTPStatusCode": 400,
+                        "HTTPHeaders": {
+                            "x-amzn-requestid": "ffffffff-ffff-ffff-ffff-ffffffffffff",
+                            "content-type": "application/x-amz-json-1.1",
+                            "content-length": "111",
+                            "date": "Sun, 01 Jan 2023 00:00:00 GMT",
+                            "connection": "close",
+                        },
+                        "RetryAttempts": 0,
+                    },
+                    "Message": "The AWS account is already a member of an organization.",
+                },
+                "create_organization",
             )
-            if operation_name == "CreateOrganizationalUnit":
-                raise ClientError(
-                    {
-                        "Error": {
-                            "Message": "The OU already exists.",
-                            "Code": "ParentNotFoundException",
-                        },
-                        "ResponseMetadata": {
-                            "RequestId": "ffffffff-ffff-ffff-ffff-ffffffffffff",
-                            "HTTPStatusCode": 400,
-                            "HTTPHeaders": {
-                                "x-amzn-requestid": "ffffffff-ffff-ffff-ffff-ffffffffffff",
-                                "content-type": "application/x-amz-json-1.1",
-                                "content-length": "111",
-                                "date": "Sun, 01 Jan 2023 00:00:00 GMT",
-                                "connection": "close",
-                            },
-                            "RetryAttempts": 0,
-                        },
+        if operation_name == "CreateOrganizationalUnit":
+            raise ClientError(
+                {
+                    "Error": {
                         "Message": "The OU already exists.",
+                        "Code": "ParentNotFoundException",
                     },
-                    "create_organizational_unit",
+                    "ResponseMetadata": {
+                        "RequestId": "ffffffff-ffff-ffff-ffff-ffffffffffff",
+                        "HTTPStatusCode": 400,
+                        "HTTPHeaders": {
+                            "x-amzn-requestid": "ffffffff-ffff-ffff-ffff-ffffffffffff",
+                            "content-type": "application/x-amz-json-1.1",
+                            "content-length": "111",
+                            "date": "Sun, 01 Jan 2023 00:00:00 GMT",
+                            "connection": "close",
+                        },
+                        "RetryAttempts": 0,
+                    },
+                    "Message": "The OU already exists.",
+                },
+                "create_organizational_unit",
             )
-
-
 
     @mock_organizations
     def test_create_aws_organization(self):
@@ -99,5 +96,20 @@ class AWSSyncTest(TestCase):
     def test_create_course_iteration_OU__exception(self):
         org = self.sync
         org.create_course_iteration_OU(1)
+        self.assertTrue(org.fail)
+        self.assertIsNone(org.org_info)
+
+    @mock_organizations
+    def test_create_team_OU(self):
+        moto_client = self.client = boto3.client("organizations")
+        org = self.sync
+        org.create_team_OU("team1")
+        describe_org = moto_client.describe_organization()["Organization"]
+        self.assertEqual(describe_org, org.org_info)
+
+    @patch("botocore.client.BaseClient._make_api_call", mock_api)
+    def test_create_team_OU__exception(self):
+        org = self.sync
+        org.create_team_OU("team1")
         self.assertTrue(org.fail)
         self.assertIsNone(org.org_info)
