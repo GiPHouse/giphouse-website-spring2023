b7b02845c7d73c78f883536e5c0cdc0262b39337
Mitchell <boesveld.mitchell@gmail.com>
2023-03-14 14:45:01 +0100

    changed generate_aws_sync_list to use SyncData structure

--- website/projects/tests/test_awssync.py
+++ website/projects/tests/test_awssync.py
@@ -130,11 +130,16 @@ class AWSSyncTest(TestCase):
         self.assertIsNone(org.org_info)
 
 
-class AWSSyncList(TestCase):
+class AWSSyncListTest(TestCase):
     """Test AWSSyncList class."""
 
     def setUp(self):
         self.sync = awssync.AWSSync()
+        self.syncData = awssync.SyncData
+
+        self.test1 = self.syncData("test1@test1.test1", "test1", "test1")
+        self.test2 = self.syncData("test2@test2.test2", "test2", "test2")
+        self.test3 = self.syncData("test3@test3.test3", "test3", "test3")
 
     def test_AWS_sync_list_both_empty(self):
         gip_list = []
@@ -142,21 +147,16 @@ class AWSSyncList(TestCase):
         self.assertEquals(self.sync.generate_aws_sync_list(gip_list, aws_list), [])
 
     def test_AWS_sync_list_empty_AWS(self):
-        gip_list = [("test1@test.test", 37), ("test2@test.test", 37), ("test3@test.test", 37)]
+        gip_list = [self.test1, self.test2]
         aws_list = []
         self.assertEquals(self.sync.generate_aws_sync_list(gip_list, aws_list), gip_list)
 
     def test_AWS_sync_list_empty_GiP(self):
         gip_list = []
-        aws_list = [("test1@test.test", 37), ("test2@test.test", 37)]
+        aws_list = [self.test1, self.test2]
         self.assertEquals(self.sync.generate_aws_sync_list(gip_list, aws_list), [])
 
-    def test_AWS_sync_list_diff_email(self):
-        gip_list = [("test1@test.test", 37), ("test2@test.test", 37), ("test3@test.test", 37)]
-        aws_list = [("test1@test.test", 37), ("test2@test.test", 37)]
-        self.assertEquals(self.sync.generate_aws_sync_list(gip_list, aws_list), [("test3@test.test", 37)])
-
-    def test_AWS_sync_list_diff_id(self):
-        gip_list = [("test1@test.test", 37), ("test1@test.test", 38), ("test1@test.test", 39)]
-        aws_list = [("test1@test.test", 37), ("test1@test.test", 38)]
-        self.assertEquals(self.sync.generate_aws_sync_list(gip_list, aws_list), [("test1@test.test", 39)])
+    def test_AWS_sync_list_both_full(self):
+        gip_list = [self.test1, self.test2]
+        aws_list = [self.test2, self.test3]
+        self.assertEquals(self.sync.generate_aws_sync_list(gip_list, aws_list), [self.test1])
