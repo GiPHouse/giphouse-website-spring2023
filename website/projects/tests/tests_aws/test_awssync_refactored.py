8fd3ba4eeb5e74f07f183354378edd280500590e
1058274 <70607431+1058274@users.noreply.github.com>
2023-05-10 22:00:52 +0200

    Fix test to check name and ID for single OU instead of over possibly multiple OUs

--- website/projects/tests/tests_aws/test_awssync_refactored.py
+++ website/projects/tests/tests_aws/test_awssync_refactored.py
@@ -21,7 +21,6 @@ class AWSSyncRefactoredTest(TestCase):
 
     def test_create_course_ou__new(self):
         self.sync.api_talker.create_organization(feature_set="ALL")
-
         root_id = self.sync.api_talker.list_roots()[0]["Id"]
         tree = AWSTree("root", root_id, [])
         current_semester_name = "Spring 2023"
@@ -29,12 +28,12 @@ class AWSSyncRefactoredTest(TestCase):
         with patch.object(Semester.objects, "get_or_create_current_semester", return_value=current_semester_name):
             course_ou_id = self.sync.create_course_ou(tree)
 
-        ous = self.sync.api_talker.list_organizational_units_for_parent(root_id)
-        ou_ids = [ou["Id"] for ou in ous]
-        ou_names = [ou["Name"] for ou in ous]
+        course_ou_exists = any(
+            ou["Id"] == course_ou_id and ou["Name"] == current_semester_name
+            for ou in self.sync.api_talker.list_organizational_units_for_parent(root_id)
+        )
 
-        self.assertIn(course_ou_id, ou_ids)
-        self.assertIn(current_semester_name, ou_names)
+        self.assertTrue(course_ou_exists)
 
     def test_create_course_ou__already_exists(self):
         tree = AWSTree(
